5b84475c5a503fd727ff41d81670427d08b1d2cf
GeorgeMoroz1 <yura0991@mail.ru>
2022-12-29 01:46:05 +0300

    bin added

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,10 @@ public class Main {
         Basket basket = new Basket(new String[]{"Хлеб", "Яблоки", "Молоко"}, new int[]{50, 100, 70});
         File file = new File("basket.bin");
         if (file.exists()) {
-            Basket basket2 = Basket.loadFromBinFile(file);
-            basket2.addToCart(basket.getProductNum(), basket.getAmount());
+            basket = Basket.loadFromBinFile(file);
+            basket.addToCart(basket.getProductNum(), basket.getAmount());
             basket.saveBin(file);
-            basket2.printCart();
+            basket.printCart();
 
         } else {
             basket.addToCart(basket.getProductNum(), basket.getAmount());
